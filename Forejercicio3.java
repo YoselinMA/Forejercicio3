cdbf38cddadae7b536964642c4589135832ed570
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-22 22:18:37 -0600

    Create Forejercicio3.java

---
+++ Forejercicio3.java
@@ -0,0 +1,14 @@
+public class Forejercicio3 {
+    public static void main(String[] args) {
+        byte a;
+
+        for(a = 1; a >=1; a--){
+            System.out.println("A   A+2   A+4   A+6\n");
+            System.out.println("3    5     7     9\n");
+            System.out.println("6    8     10    12\n");
+            System.out.println("9    11    13    15\n");
+            System.out.println("12   14    16    18\n");
+        }
+    }
+    
+}
